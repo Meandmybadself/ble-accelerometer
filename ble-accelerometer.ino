33f233c672f0620b43fa534dbe0b585728a4da3b
Jeffery Bennett <jeffery@permanentrecord.io>
2019-03-31 09:47:23 -0500

    Formatting.

--- ble-accelerometer.ino
+++ ble-accelerometer.ino
@@ -6,7 +6,7 @@
 #include "BluefruitConfig.h"
 
 #if SOFTWARE_SERIAL_AVAILABLE
-  #include <SoftwareSerial.h>
+#include <SoftwareSerial.h>
 #endif
 
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
@@ -34,12 +34,15 @@ int32_t gattServiceId;
 int32_t accel1CharId;
 int32_t accel2CharId;
 
-void error(const __FlashStringHelper*err) {
+void error(const __FlashStringHelper *err)
+{
   Serial.println(err);
-  while (1);
+  while (1)
+    ;
 }
 
-void setup() {
+void setup()
+{
 
   boolean success;
 
@@ -48,16 +51,17 @@ void setup() {
 
   Serial.print(F("Initialising the Bluefruit LE module: "));
 
-  if ( !ble.begin(VERBOSE_MODE) )
+  if (!ble.begin(VERBOSE_MODE))
   {
     error(F("Couldn't find Bluefruit, make sure it's in Command mode & check wiring?"));
   }
-  Serial.println( F("OK!") );
+  Serial.println(F("OK!"));
 
   /* Perform a factory reset to make sure everything is in a known state */
   Serial.println(F("Performing a factory reset: "));
-  if (! ble.factoryReset() ){
-       error(F("Couldn't factory reset"));
+  if (!ble.factoryReset())
+  {
+    error(F("Couldn't factory reset"));
   }
 
   /* Disable command echo from Bluefruit */
@@ -65,28 +69,30 @@ void setup() {
 
   Serial.println(F("Setting device name to 'BRA': "));
 
-  if (! ble.sendCommandCheckOK(F("AT+GAPDEVNAME=BRA")) ) {
+  if (!ble.sendCommandCheckOK(F("AT+GAPDEVNAME=BRA")))
+  {
     error(F("Could not set device name?"));
   }
 
   /* Add the Generic Access Service definition */
   Serial.println(F("Adding the Custom GATT Service definition: "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDSERVICE=UUID128=00-77-13-12-11-00-00-00-00-00-AB-BA-0F-A1-AF-E1"), &gattServiceId);
-  if (! success) {
+  success = ble.sendCommandWithIntReply(F("AT+GATTADDSERVICE=UUID128=00-77-13-12-11-00-00-00-00-00-AB-BA-0F-A1-AF-E1"), &gattServiceId);
+  if (!success)
+  {
     error(F("Could not add Custom service"));
   }
 
-
   Serial.println(F("Adding the Notifiable characteristic for Accelerometer 1 "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=00-68-42-01-14-88-59-77-42-42-AB-BA-0F-A1-AF-E1,PROPERTIES=0x10,MIN_LEN=1, MAX_LEN=11, VALUE=0x000000, DATATYPE=1, DESCRIPTION='Baseline Accelerometer'"), &accel1CharId);
-  if (! success) {
+  success = ble.sendCommandWithIntReply(F("AT+GATTADDCHAR=UUID128=00-68-42-01-14-88-59-77-42-42-AB-BA-0F-A1-AF-E1,PROPERTIES=0x10,MIN_LEN=1, MAX_LEN=11, VALUE=0x000000, DATATYPE=1, DESCRIPTION='Baseline Accelerometer'"), &accel1CharId);
+  if (!success)
+  {
     error(F("Could not add Accel1 characteristic"));
   }
-  
-  
+
   Serial.println(F("Adding the Notifiable characteristic for Acceleromerer 2 "));
-  success = ble.sendCommandWithIntReply( F("AT+GATTADDCHAR=UUID128=00-69-43-02-15-89-60-78-43-43-AC-BB-0F-A2-AF-E2,PROPERTIES=0x10,MIN_LEN=1, MAX_LEN=11, VALUE=0x000000, DATATYPE=1, DESCRIPTION='Delta Accelerometer'"), &accel2CharId);
-  if (! success) {
+  success = ble.sendCommandWithIntReply(F("AT+GATTADDCHAR=UUID128=00-69-43-02-15-89-60-78-43-43-AC-BB-0F-A2-AF-E2,PROPERTIES=0x10,MIN_LEN=1, MAX_LEN=11, VALUE=0x000000, DATATYPE=1, DESCRIPTION='Delta Accelerometer'"), &accel2CharId);
+  if (!success)
+  {
     error(F("Could not add Accel2 characteristic"));
   }
 
@@ -95,14 +101,14 @@ void setup() {
   ble.reset();
 
   Serial.println();
-  
 }
 
-void loop() {
+void loop()
+{
   int acc1_currX = analogRead(acc1_xpin);
   int acc1_currY = analogRead(acc1_ypin);
   int acc1_currZ = analogRead(acc1_zpin);
-  
+
   int acc2_currX = analogRead(acc2_xpin);
   int acc2_currY = analogRead(acc2_ypin);
   int acc2_currZ = analogRead(acc2_zpin);
@@ -125,5 +131,5 @@ void loop() {
   ble.print(F("|"));
   ble.println(acc2_currZ);
 
-  delay(500);
+  delay(250);
 }
